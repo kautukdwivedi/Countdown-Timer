7dfde252449dfc0c93edfaffbceab767aca96890
kautukdwivedi <kautukdwivedi@icloud.com>
2021-03-27 12:33:09 +0530

    1 sec

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ def countdown(t):
         mins,secs = divmod(t,60)
         timer = '{:02d}:{:02d}'.format(mins,secs)
         print(timer,end="\r")
-        time.sleep(2)
+        time.sleep(1)
         t-=1
     print("Timer Completed")
 
